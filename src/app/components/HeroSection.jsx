65478c43ba02b47ff449e6eee031504c309910c1
Dany Syauqi <danysyauqinazhif@upi.edu>
2024-02-16 14:31:06 +0700

    web v1.0

--- src/app/components/HeroSection.jsx
+++ src/app/components/HeroSection.jsx
@@ -7,15 +7,15 @@ import Link from "next/link";
 
 const HeroSection = () => {
   return (
-    <section className="lg:py-16 lg:px-16">
+    <section className='lg:py-16'>
       <div className="grid grid-cols-1 sm:grid-cols-12">
         <motion.div
           initial={{ opacity: 0, scale: 0.5 }}
           animate={{ opacity: 1, scale: 1 }}
           transition={{ duration: 0.5 }}
-          className="col-span-8 place-self-center text-center sm:text-left justify-self-start"
+          className="col-span-7 place-self-center text-center"
         >
-          <h1 className='text-white mb-4 text-4xl sm:text-5xl lg:text-6xl lg:leading-normal font-extrabold'>
+          <h1 className='text-white mb-4 text-4xl sm:text-5xl lg:text-6xl font-extrabold'>
             <span className="text-transparent bg-clip-text bg-gradient-to-t from-cyan-400 to-blue-600">
                Hello, I&apos;m{" "}
             </span>
@@ -28,7 +28,7 @@ const HeroSection = () => {
                 1000,
                 'Data Engineer',
                 1000,
-                'Data Science',
+                'Data Scientist',
                 1000
               ]}
               wrapper="span"
